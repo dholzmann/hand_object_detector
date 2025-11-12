1c3c06e4fc133b7c244e20ede9cd93de247f4394
Dennis Holzmann <dennis.holzmann@uni-bielefeld.de>
2025-11-12 16:06:00 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hoi",
+    version="1.0.1",
+    description="Hand Object Interaction Detector",
+    python_requires=">=3.5.0",
+    packages = find_packages(),
+)
